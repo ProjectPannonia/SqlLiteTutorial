6ce667a1b0842404d650c39c24b8280e4856fc9f
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-03-10 21:04:28 +0100

    Add project files.

---
+++ SqlLiteTutorial/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace SqlLiteTutorial
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
